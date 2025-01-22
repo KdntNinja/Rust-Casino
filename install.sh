94c90909182e536fadffbe360a5c7cecc5121b32
KdntNinja <116434277+KdntNinja@users.noreply.github.com>
2025-01-22 12:05:21 +0000

    Enhance installation script and refactor main function to include password protection

--- install.sh
+++ install.sh
@@ -1,1 +1,3 @@
+clear
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+echo "export PATH=\$HOME/.cargo/bin:\$PATH" >> ~/.bashrc
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,25 @@
 use std::io;
 
+const PASSWORD: &str = "password";
+
 fn main() {
-    loop {
-        let mut number: String = String::new();
-        println!("Enter a number: ");
-        io::stdin().read_line(&mut number).expect("Failed to read line");
-
-        let mut power: String = String::new();
-        println!("Enter the power: ");
-        io::stdin().read_line(&mut power).expect("Failed to read line");
-
-        let number: i64 = number.trim().parse().expect("Please type a number!");
-        let power: u32 = power.trim().parse().expect("Please type a number!");
-        let answer: i64 = number.pow(power);
-        print!("{} to the power of {} is: {}", number, power, answer);
-
-        let mut choice: String = String::new();
-        println!("\nDo you want to continue? (y/n)");
-        io::stdin().read_line(&mut choice).expect("Failed to read line");
-
-        if choice.trim() == "y" {
-            continue;
-        } else if choice.trim() == "n" {
+    let mut attempts: i16 = 0;
+    while attempts < 3 {
+        if check_password() {
+            println!("Correct password!");
             break;
         } else {
-            println!("Invalid choice. Exiting the program.");
-            break;
+            println!("Incorrect password! {} attempts left", 2 - attempts);
+            println!();
+            attempts += 1;
         }
     }
+}
+
+fn check_password() -> bool {
+    println!("Enter the password: ");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let input = input.trim();
+    input == PASSWORD
 }
