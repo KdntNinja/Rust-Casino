281cb1602221f69b8431cfd5f34bfc464f33c365
KdntNinja <116434277+KdntNinja@users.noreply.github.com>
2025-01-29 16:53:18 +0000

    Add Rust components for enhanced development support

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 clear
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rustup component add rust-analyzer
+rustup component add rls
 echo "export PATH=\$HOME/.cargo/bin:\$PATH" >> ~/.bashrc
